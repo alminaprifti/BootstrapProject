ab1e9424e37fe1c8b7917216512e1c486c09899f
Alma Prifti <prifti.alma@yahoo.com>
2020-12-05 21:35:25 -0500

    Project

--- js/script.js
+++ js/script.js
@@ -23,3 +23,53 @@ function newYear() {
 setInterval(function () {
   newYear();
 }, 1000);
+
+$(function(){
+
+  $("#shopcart-items").slideUp();
+  $(".cart").on("click", function () {
+  $("#shopcart-items").slideToggle();
+  });
+
+  $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+
+  
+  $(".item").on("click", function () {
+    $("#shopcart-items").slideDown();
+   setTimeout(function(){
+      $("#shopcart-items").slideUp();
+   }, 2000)
+    //add items to basket
+    $(this).each(function () {
+      var name = $(this).children(".item-detail").children("h4").text();
+      var remove = "<button class='remove'> X </button>";
+      var cena = "<span class='eachPrice'>" + (parseFloat($(this).children(".item-detail").children(".prices").children(".price").text())) + "</span>";
+      $("#list-item").append("<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>");
+
+      //number of items in basket
+      $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+      $("#items-basket").text();
+      
+        //calculate total price
+        var totalPrice = 0;
+          $(".eachPrice").each(function (){ 
+            var cenaEach = parseFloat($(this).text());
+            totalPrice+=cenaEach;
+          });
+          $("#total-price").text(totalPrice + "$");
+    });
+
+    //remove items from basket
+    $(".remove").on("click", function () {
+      $(this).parent().remove();
+
+          var totalPrice = 0;
+          $(".eachPrice").each(function (){ 
+            var cenaEach = parseFloat($(this).text());
+            totalPrice+=cenaEach;
+          });
+          $("#total-price").text(totalPrice + "$");
+      $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+    });
+  });
+})
